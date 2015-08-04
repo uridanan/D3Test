3529538915dab1e05f32784efc2354a4ccc43e07
uridanan <uridanan@gmail.com>
2015-08-04 08:03:44 +0300

    Flatten the nest
    Figured out how to map the nest to the proper format
    Investigating how to fill in blank dates

--- jessyData.js
+++ jessyData.js
@@ -35,23 +35,49 @@ function jessyData(data){
         .key(function(d){return d["Intake Type"]; })
         .key(function(d){return d["Intake Date"];}).sortKeys(d3.ascending)
         .rollup(function(leaves) {return leaves.length;})
-        //.rollup(function(leaves) { return {"value": leaves.length, "date": leaves[0]["Intake Date"]};})
         .entries(data);
 
-    var flat = nest.entries().map(function(d){
-        return d.value.entries().map(function(e){
-            return {
-                "date": e.key,
-                "value": e.values,
-                "key": d.key
-            };
-        })
+    //Fix the layers to fit the stack
+    var flat = nest.map(function(d){
+        return{
+            "key": d.key,
+            "values": d.values.map(function(f){
+                return {
+                    "date": f.key,
+                    "value": f.values,
+                    "key": d.key
+                };
+            })
+        };
     }).reduce(function(d1,d2){ return d1.concat(d2) },[]);
 
-    return flat;
+    //return flat;
 
+    //Array.apply(null, Array(5)).map(Number.prototype.valueOf,0);
     //http://stackoverflow.com/questions/14270011/flatten-an-object-hierarchy-created-with-d3-js-nesting
     //http://stackoverflow.com/questions/27589618/flatten-nested-array-created-with-d3-nest-to-create-stacked-bar
+    //http://stackoverflow.com/questions/17415406/filling-the-gaps-in-d3-array-nesting
 
     //Next: fill in blank dates
+    var dates = d3.nest()
+        .key(function(d){return d["Intake Date"];}).sortKeys(d3.ascending)
+        .rollup(function(leaves) {return 0;})
+        .entries(data);
+
+    var makeAllKeys = function(day) {
+        allKeys = [];
+        for(var i = 0; i<365; i++) {
+            allKeys.push(yMFormat(new Date(year,i,1)));
+        }
+        return allKeys;
+    }
+
+    nestedData = nestedData.map(function(yearObj) {
+        return {
+            values: makeAllKeys(+yearObj.key).map(function(k) {
+                value = yearObj.values.filter(function(v) { return v.key == k; })[0];
+                return value || ({key: k, values: 0});
+            })
+        };
+    });
 }
